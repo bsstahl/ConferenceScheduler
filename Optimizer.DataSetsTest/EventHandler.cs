20c017af5b4f3c18fdaaef0cba40e3848ad3ba5e
bsstahl <barry@bsstahl.com>
2017-10-01 23:05:07 -0700

    Update to version 1.0.1 to fix dependency issues for the nuget package(s)

--- Optimizer.DataSetsTest/EventHandler.cs
+++ Optimizer.DataSetsTest/EventHandler.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using ConferenceScheduler.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConferenceScheduler.Entities;
 
 namespace Optimizer.DataSetsTest
 {
